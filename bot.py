3ad2a055de215f9d7c24f40a74e0a817b8bcdb11
Niklas Meinzer <github@niklas-meinzer.de>
2021-09-10 12:33:58 +0200

    Fix parser and use click

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
-from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
-import requests
-from tinydb import TinyDB, Query
-from bs4 import BeautifulSoup
+import logging
 import sys
+
+import click
+import requests
+from bs4 import BeautifulSoup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from tinydb import Query, TinyDB
 
 db = TinyDB("db.json")
 Job = Query()
@@ -16,22 +19,22 @@ class JobExistsException(Exception):
 
 def parse_result_item(item):
     """
-        Takes a li item containing one search result and parses id, url and price from it.
-        Returns a dict containing the results.
+    Takes a li item containing one search result and parses id, url and price from it.
+    Returns a dict containing the results.
     """
     main = item.find_all("div", {"aditem-main"})
-    details = item.find_all("div", {"aditem-details"})
+    price = item.find_all("p", {"aditem-main--middle--price"})
     article = item.find_all("article")
 
-    if len(main) != 1 or len(article) != 1 or len(details) != 1:
+    if len(main) != 1 or len(article) != 1 or len(price) != 1:
         return
     main = main[0]
     article = article[0]
-    details = details[0]
+    price = price[0]
 
     result = {
         "ad_id": article["data-adid"],
-        "price": details.find_all("strong")[0].text,
+        "price": price.text.strip(),
     }
 
     a = main.find_all("a")[0]
@@ -42,17 +45,17 @@ def parse_result_item(item):
 
 def execute_search(search_term):
     """
-        Runs the search for one search term.
-        Returns a list containing all parsed search results.
+    Runs the search for one search term.
+    Returns a list containing all parsed search results.
     """
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
     }
-    url = f"https://www.ebay-kleinanzeigen.de/s-79102/{search_term}/k0l9364r20"
+    url = f"https://www.ebay-kleinanzeigen.de/s-79249/{search_term}/k0l9364r20"
 
     response = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(response.content)
+    soup = BeautifulSoup(response.content, features="html.parser")
 
     ul = soup.find_all("ul", {"id": "srchrslt-adtable"})
     assert len(ul) == 1
@@ -66,14 +69,17 @@ def execute_search(search_term):
         data = parse_result_item(i)
         if data is not None:
             results.append(data)
-
+    if len(results) == 0:
+        logging.warning(
+            f"No results found for search term '{search_term}'. Check if parser works correctly."
+        )
     return results
 
 
 def init_search(search_term, chat_id):
     """
-        Initialize a new search term.
-        Executes one search and marks all current results as known.
+    Initialize a new search term.
+    Executes one search and marks all current results as known.
     """
     result = db.search(Job.search_term == search_term)
 
@@ -85,19 +91,13 @@ def init_search(search_term, chat_id):
     db.insert({"search_term": search_term, "chat_id": chat_id, "known_ads": ids})
 
 
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
-    )
-
-
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
 def start_watching(update, context):
     """
-        Command handler for starting to watch a new search term.
+    Command handler for starting to watch a new search term.
     """
     search_target = "".join(context.args)
     try:
@@ -111,7 +111,7 @@ def start_watching(update, context):
 
 def stop_watching(update, context):
     """
-        Command handler for stopping to watch a search term
+    Command handler for stopping to watch a search term
     """
     search_term = "".join(context.args)
     result = db.search(Job.search_term == search_term)
@@ -126,7 +126,7 @@ def stop_watching(update, context):
 
 def look_for_stuff(context):
     """
-        Command handler to peridically check all active search jobs.
+    Command handler to peridically check all active search jobs.
     """
     for job in db.all():
         known_ads = set(job["known_ads"])
@@ -157,8 +157,15 @@ def status(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-if __name__ == "__main__":
-    TELEGRAM_BOT_TOKEN = sys.argv[1]
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--token", prompt=True, help="The telegram bot api token")
+def run(token):
+    TELEGRAM_BOT_TOKEN = token
 
     updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
@@ -166,19 +173,27 @@ if __name__ == "__main__":
 
     job_minute = job_queue.run_repeating(look_for_stuff, interval=5 * 60, first=0)
 
-    start_handler = CommandHandler("start", start)
-    dispatcher.add_handler(start_handler)
-
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dispatcher.add_handler(echo_handler)
 
-    start_watching_handler = CommandHandler("start_watching", start_watching)
+    start_watching_handler = CommandHandler("start", start_watching)
     dispatcher.add_handler(start_watching_handler)
 
-    stop_handler = CommandHandler("stop_watching", stop_watching)
+    stop_handler = CommandHandler("stop", stop_watching)
     dispatcher.add_handler(stop_handler)
 
     status_handler = CommandHandler("status", status)
     dispatcher.add_handler(status_handler)
 
     updater.start_polling()
+
+
+@cli.command()
+@click.argument("searchterm")
+def search(searchterm):
+    data = execute_search(searchterm)
+    click.echo(data)
+
+
+if __name__ == "__main__":
+    cli()
